3e59eed7acd24defae818f0bf34d820e328d0e2f
Gaurav yadav <103619206+gaurav93269@users.noreply.github.com>
2022-07-14 17:37:28 +0530

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,13 @@
+//what is class?
+
+public class demo   // The class is one of the defining ideas of oops.
+{
+    public static void main(String[] args) 
+    {
+         /*collection of object is called class.
+              eg. Class -color
+                   red,orange,green - an object of color*/
+    	
+       System.out.println("welcome to class");
+    }
+}
